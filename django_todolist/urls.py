c2ad1960c84f8f3da75733575b9b6f4df717c8a5
kuro_ye <erhanaket77@gmail.com>
2022-01-18 19:45:28 +0600

    add new urls

--- django_todolist/urls.py
+++ django_todolist/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django_todolist import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('', include('todolist.urls'))
-
+    path('', include('todolist.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+
