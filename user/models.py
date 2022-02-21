de7ad2822cb1f6b5cd0203be7e0e382daf35f3cf
kuro_ye <erhanaket77@gmail.com>
2022-02-21 16:15:49 +0600

    fix: fix default value

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    level = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
     xp = models.IntegerField(default=0)
 
     class Meta:
