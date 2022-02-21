e3bc20e16d5bcd57bd036a8b414ee006b9a7ea2d
kuro_ye <erhanaket77@gmail.com>
2022-02-21 15:24:16 +0600

    update: :hammer: add new attr xp

--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     level = models.IntegerField(default=0)
+    xp = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'user'
