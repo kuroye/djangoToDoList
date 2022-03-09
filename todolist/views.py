46e702996d9b9930d043c7476e38620a38265eb4
kuro_ye <erhanaket77@gmail.com>
2022-03-09 21:09:18 +0600

    get functions from utils.data

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,7 @@
-from datetime import timedelta
-
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from todolist.models import Todo, User
-import random
-
+from utils.data import *
 
 # Create your views here.
 @login_required
@@ -64,42 +61,3 @@ def index(request):
             return redirect(reverse('index'))
 
 
-def justify_time(todos):
-    for todo in todos:
-        todo.create_date = todo.create_date + timedelta(hours=6)
-        todo.save()
-
-
-def level_up(user, max_xp):
-    user.level = user.level + 1
-
-    if user.xp >= max_xp:
-        user.xp = user.xp - max_xp
-
-    user.save()
-
-
-def calculate_max_xp(level):
-    max_xp = pow(level, 2) * 100
-    return max_xp
-
-
-def evaluate_xp(difficulty):
-    if difficulty == 'easy':
-        return random.randint(1, 20)
-    elif difficulty == 'medium':
-        return random.randint(20, 100)
-    elif difficulty == 'hard':
-        return random.randint(100, 500)
-    elif difficulty is None:
-        return 0
-
-def evaluate_coin(difficulty):
-    if difficulty == 'easy':
-        return 0
-    elif difficulty == 'medium':
-        return random.randint(1, 10)
-    elif difficulty == 'hard':
-        return random.randint(10, 50)
-    elif difficulty is None:
-        return 0
