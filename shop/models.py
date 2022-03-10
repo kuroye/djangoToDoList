55c62a9ce7ef51ea343c03c2f228d5e7e776c12f
kuro_ye <erhanaket77@gmail.com>
2022-03-10 15:52:02 +0600

    add new attr

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=500, null=True, blank=True)
     price = models.IntegerField(default=10)
-    grade = models.CharField(max_length=50, default='inferior')
+    requirement = models.IntegerField(default=0)
+    grade = models.CharField(max_length=50, default='copper')
 
 
