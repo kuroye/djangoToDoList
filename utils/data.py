cd8664750596e59b8be67c82ce30831ab48a63ff
kuro_ye <erhanaket77@gmail.com>
2022-03-11 14:32:18 +0600

    add evaluate price function

--- utils/data.py
+++ utils/data.py
@@ -41,3 +41,23 @@ def evaluate_coin(difficulty):
         return random.randint(10, 50)
     elif difficulty is None:
         return 0
+
+def evaluate_price(grade):
+    if grade == 'copper':
+        return random.randint(1, 10)
+    elif grade == 'silver':
+        return random.randint(10, 50)
+    elif grade == 'gold':
+        return random.randint(50, 100)
+    elif grade == 'platinum':
+        return random.randint(100, 300)
+    elif grade == 'diamond':
+        return random.randint(300, 500)
+    elif grade == 'crown':
+        return random.randint(500, 750)
+    elif grade == 'king':
+        return random.randint(750, 900)
+    elif grade == 'legend':
+        return random.randint(900, 1000)
+    elif grade is None:
+        return 0
