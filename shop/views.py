99d3fc7f9e80843bb04367c8e118e9ca84c7a433
kuro_ye <erhanaket77@gmail.com>
2022-03-11 14:36:03 +0600

    POST request

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from utils.data import *
+from .models import Item
 # Create your views here.
 
 def shop(request):
@@ -11,3 +12,16 @@ def shop(request):
         max_xp = calculate_max_xp(current_level)
 
         return render(request, 'shop.html', context={'max_xp': max_xp})
+
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        requirement = request.POST.get('requirement')
+        grade = request.POST.get('grade')
+        price = evaluate_price(grade)
+
+        Item.objects.create(name=title, description=description, requirement=requirement,
+                            grade=grade, price=price)
+
+        return redirect(reverse('shop'), kwargs={'message': 'Created successful'})
