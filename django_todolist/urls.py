b2de85f80317feecf827273be9ff5cf3620df16b
kuro_ye <erhanaket77@gmail.com>
2022-03-12 20:38:32 +0600

    urlpatterns

--- django_todolist/urls.py
+++ django_todolist/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include, re_path
 from django.views.static import serve
 from django_todolist import settings
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
@@ -27,3 +30,4 @@ urlpatterns = [
     # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
