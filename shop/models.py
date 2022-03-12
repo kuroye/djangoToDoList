2a984dd843520a540a648fb70fc1209ad2625cd1
kuro_ye <erhanaket77@gmail.com>
2022-03-12 20:37:15 +0600

    image attr

--- shop/models.py
+++ shop/models.py
@@ -7,5 +7,6 @@ class Item(models.Model):
     price = models.IntegerField(default=10)
     requirement = models.IntegerField(default=0)
     grade = models.CharField(max_length=50, default='copper')
+    image = models.ImageField(null=True, blank=True)
 
 
