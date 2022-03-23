1bb17602ee6a7d1b67b795708320e3ff68c9d49e
kuro_ye <erhanaket77@gmail.com>
2022-03-23 18:25:44 +0600

    delete unnecessary part

--- divider/views.py
+++ divider/views.py
@@ -11,24 +11,11 @@ def shop(request):
         return render(request, 'shop.html', context={'money': current_money})
 
     if request.method == 'POST':
-        # form = ItemForm(request.POST, request.FILES)
-        #
-        # if form.is_valid():
+
         title = request.POST.get('title')
         description = request.POST.get('description')
         requirement = request.POST.get('requirement')
         grade = request.POST.get('grade')
-        # image = form.
         price = evaluate_price(grade)
-
-
-
-        # last_image = Item.objects.last()
-        #
-        # last_image.path = last_image.image.url
-        # last_image.save()
-
-    Item.objects.create(name=title, description=description, requirement=requirement,
-                        grade=grade, price=price)
 
     return redirect(reverse('shop'), kwargs={'message': 'Created successful'})
