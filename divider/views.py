29cf4470405323b077832690da3c4eacc502aef0
kuro_ye <erhanaket77@gmail.com>
2022-03-27 09:59:06 +0600

    calculate parts

--- divider/views.py
+++ divider/views.py
@@ -7,8 +7,14 @@ def divider(request):
     if request.method == 'GET':
         user = request.user
         current_money = user.money
+        money_need = current_money*0.5
+        money_invest = current_money*0.4
+        money_want = current_money*0.1
 
-        return render(request, 'divider.html', context={'money': current_money})
+        return render(request, 'divider.html', context={'money': current_money,
+                                                        'need': money_need,
+                                                        'invest': money_invest,
+                                                        'want': money_want})
 
     if request.method == 'POST':
         user = request.user
